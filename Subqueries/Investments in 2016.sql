a3f3d261f3412c20403f751c54fd1f05bb35258a
Tuhin <42709485+tuhinmanna@users.noreply.github.com>
2026-01-20 22:00:29 +0530

    Create Investments in 2016.sql

---
+++ Subqueries/Investments in 2016.sql
@@ -0,0 +1,9 @@
+select round(sum(tiv_2016),2) as tiv_2016
+from Insurance i1 
+where i1.tiv_2015 in (
+    select tiv_2015 from Insurance group by tiv_2015 having count(*) > 1
+)
+and 
+(i1.lat, i1.lon) in (
+    select lat,lon from Insurance group by lat, lon having count(*)  = 1
+)
